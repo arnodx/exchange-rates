4da34594f4ce3377cae013a823212db1cc89fde7
adamus <129192700+arnodx@users.noreply.github.com>
2023-03-28 19:47:53 +0200

    add dark theme and filters

--- src/app/modules/main/main.component.ts
+++ src/app/modules/main/main.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {ExchangeRatesService} from "../../core/services/exchange-rates.service";
 import {Rates} from "../../core/models/exchange-rates";
 import {map} from "rxjs/operators";
+import {ThemeService} from "../../core/services/theme-service.service";
+import * as moment from "moment";
 
 @Component({
   selector: 'app-main',
@@ -12,8 +14,19 @@ export class MainComponent implements OnInit {
   exchangeRates!: Rates[];
   first = 0;
   rows = 10;
+  stateOptions: any[];
+  darkTheme: string = 'off';
+  dateFilter!:Date;
+  constructor(
+    private exchangeRatesService: ExchangeRatesService,
+    private themeService: ThemeService,
+  ) {
+    this.stateOptions = [
+      { label: 'Ciemny', value: 'md-dark' },
+      { label: 'Jasny', value: 'md-light' },
+    ];
+  }
 
-  constructor(private exchangeRatesService: ExchangeRatesService) { }
 
   ngOnInit() {
     this.exchangeRatesService.getExchangeRatesForToday().pipe(
@@ -39,5 +52,23 @@ export class MainComponent implements OnInit {
 
   isFirstPage(): boolean {
     return this.exchangeRates ? this.first === 0 : true;
+  }
+
+  changeTheme(event:any) {
+    this.themeService.switchTheme(event.value);
+  }
+  filterExchangeRateByDate(date:Date) {
+    if(moment(date).isValid()){
+      const formatedDate = moment(date).format("YYYY-MM-DD")
+      this.exchangeRatesService.getExchangeRatesByDate(formatedDate).pipe(
+        map(exchangeRate => exchangeRate[0].rates)
+      ).subscribe(exchangeRate =>this.exchangeRates=exchangeRate)
+    }
+  }
+
+  clearFilterExchangeRate() {
+    this.exchangeRatesService.getExchangeRatesForToday().pipe(
+      map(exchangeRate => exchangeRate[0].rates)
+    ).subscribe(exchangeRate =>this.exchangeRates=exchangeRate)
   }
 }
--- src/app/primeng-modules.ts
+++ src/app/primeng-modules.ts
@@ -1,7 +1,11 @@
 import {TableModule} from "primeng/table";
 import {ButtonModule} from "primeng/button";
+import {SelectButtonModule} from "primeng/selectbutton";
+import {CalendarModule} from "primeng/calendar";
 
 export const PRIMENG_MODULES = [
   TableModule,
   ButtonModule,
+  SelectButtonModule,
+  CalendarModule
 ]
